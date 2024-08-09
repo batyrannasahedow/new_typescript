4ea303538bdb26e701e5c98a0a4ecf59e45e0d62
batyrannasahedow <batyrannasahedow5@gmail.com>
2024-08-09 14:09:47 +0500

    second commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,15 @@ import "../App.css";
 import { CiSearch } from "react-icons/ci";
 import { LiaLocationArrowSolid } from "react-icons/lia";
 import {  Root } from '../Types/Type';
+import dayjs  from "dayjs"
 
 function Header (): JSX.Element {
   const inputRef = useRef();
   const [term, setTerm] = React.useState<string>("");
-  const [forecast, setForecast] = React.useState<Root | null>(null)
+  const [forecast, setForecast] = React.useState<Root | null>(null);
+  const [weather, setWeather] = React.useState<boolean>(false);
+  const [weather2, setWeather2] = React.useState<boolean>(false);
+  const [weather3, setWeather3] = React.useState<boolean>(false);
 const api_key = "719626185a0744c8ad783157242907";
 const api_Endpoint = "https://api.weatherapi.com/v1/";
 
@@ -27,12 +31,19 @@ const getForecast = async (city : string) => {
   .then((res) => res.json())
   .then((res) => setForecast(res))
 }
+ const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) =>{
+  if (e.key === 'Enter') {
+    getForecast(inputRef.current.value);
+  }
+ }
+
+
 
   return (
     <div className="Header">
       <div className="div-header">
         <CiSearch className="search-icon" onClick={() => getForecast(inputRef.current.value)} />
-        <input ref={inputRef} type="text" placeholder="Search city" className="search-input" value={term} onChange={onInputChange} />
+        <input ref={inputRef} type="text" placeholder="Search city" className="search-input" value={term} onChange={onInputChange} onKeyDown={handleKeyPress}/>
       </div>
       <div className="div2-header" >
         <img className='img-icon' src={`${forecast?.current.condition.icon}`} /> 
@@ -41,63 +52,150 @@ const getForecast = async (city : string) => {
         <span className="location-icon">{forecast?.location.country}, {forecast?.location.name}</span>
         <span className="location-icon"></span>
         <p className="gradus">{forecast?.current.temp_c} ℃</p>
-        <span className="sagat-gun" >{forecast?.current.is_day}, {forecast?.current.last_updated}</span>
+        <span className="sagat-gun" >{dayjs(forecast?.current.is_day).format('dddd')}, {dayjs(forecast?.current.last_updated).format('HH:mm A')} </span>
         </div>
       </div>
-
-      <div className="div3-header">
         <hr className="hr-icon"  />
         <h3 className="chaklama">The Next Day Forecast</h3>
-        <div className="div4-header" >
-        <button className="span-days1" >2 days</button>
-        <button className="span-days2">5 days</button>
-        <button className="span-days3">10 days</button>
-        </div>
-        <div className="div4-header" >
+        <div className="div3-header">
+        {
+          weather?<div> <div className="div4-header" >
         <img className="storm-icon"src={`${forecast?.forecast.forecastday[1].hour[9].condition.icon}`} />
-          <span className="span-tuesday">{forecast?.forecast.forecastday[1].hour[9].is_day}, {forecast?.forecast.forecastday[1].date}</span> <br />
+          <span className="span-tuesday">{dayjs(forecast?.forecast.forecastday[1].hour[9].is_day).format('dddd')}, {dayjs(forecast?.forecast.forecastday[1].date).format('MMM DD')}</span> <br />
           <span className="span-storm">{forecast?.forecast.forecastday[1].hour[9].condition.text}</span>
         </div>
         <div className="div4-header">
           <img className="storm-icon"src={`${forecast?.forecast.forecastday[2].hour[9].condition.icon}`} />
-          <span className="span-tuesday">{forecast?.forecast.forecastday[2].hour[9].is_day}, {forecast?.forecast.forecastday[2].date}</span> <br />
+          <span className="span-tuesday">{dayjs(forecast?.forecast.forecastday[2].hour[9].is_day).format('dddd')}, {dayjs(forecast?.forecast.forecastday[2].date).format("MMM DD")}</span> <br />
           <span className="span-storm">{forecast?.forecast.forecastday[2].hour[9].condition.text}</span>
         </div>
-      </div>
+          </div> :<button className="span-days1" onClick={() => setWeather(true)} >2 days</button>
+        }
+         {
+          weather2?<div className='div9-header' > <div className="div4-header" >
+        <img className="storm-icon"src={`${forecast?.forecast.forecastday[1].hour[9].condition.icon}`} />
+          <span className="span-tuesday">{dayjs(forecast?.forecast.forecastday[1].hour[9].is_day).format('dddd')}, {dayjs(forecast?.forecast.forecastday[1].date).format('MMM DD')}</span> <br />
+          <span className="span-storm">{forecast?.forecast.forecastday[1].hour[9].condition.text}</span>
+        </div>
+        <div className="div4-header">
+          <img className="storm-icon"src={`${forecast?.forecast.forecastday[2].hour[9].condition.icon}`} />
+          <span className="span-tuesday">{dayjs(forecast?.forecast.forecastday[2].hour[9].is_day).format('dddd')}, {dayjs(forecast?.forecast.forecastday[2].date).format("MMM DD")}</span> <br />
+          <span className="span-storm">{forecast?.forecast.forecastday[2].hour[9].condition.text}</span>
+        </div>
+        <div className="div4-header">
+          <img className="storm-icon"src={`${forecast?.forecast.forecastday[3].hour[9].condition.icon}`} />
+          <span className="span-tuesday">{dayjs(forecast?.forecast.forecastday[3].hour[9].is_day).format('dddd')}, {dayjs(forecast?.forecast.forecastday[3].date).format("MMM DD")}</span> <br />
+          <span className="span-storm">{forecast?.forecast.forecastday[3].hour[9].condition.text}</span>
+        </div>
+        <div className="div4-header">
+          <img className="storm-icon"src={`${forecast?.forecast.forecastday[4].hour[9].condition.icon}`} />
+          <span className="span-tuesday">{dayjs(forecast?.forecast.forecastday[4].hour[9].is_day).format('dddd')}, {dayjs(forecast?.forecast.forecastday[4].date).format("MMM DD")}</span> <br />
+          <span className="span-storm">{forecast?.forecast.forecastday[4].hour[9].condition.text}</span>
+        </div>
+        <div className="div4-header">
+          <img className="storm-icon"src={`${forecast?.forecast.forecastday[5].hour[9].condition.icon}`} />
+          <span className="span-tuesday">{dayjs(forecast?.forecast.forecastday[5].hour[9].is_day).format('dddd')}, {dayjs(forecast?.forecast.forecastday[5].date).format("MMM DD")}</span> <br />
+          <span className="span-storm">{forecast?.forecast.forecastday[5].hour[9].condition.text}</span>
+        </div>
+        <div className="div4-header">
+          <img className="storm-icon"src={`${forecast?.forecast.forecastday[6].hour[9].condition.icon}`} />
+          <span className="span-tuesday">{dayjs(forecast?.forecast.forecastday[6].hour[9].is_day).format('dddd')}, {dayjs(forecast?.forecast.forecastday[6].date).format("MMM DD")}</span> <br />
+          <span className="span-storm">{forecast?.forecast.forecastday[6].hour[9].condition.text}</span>
+        </div>
+        <div className="div4-header">
+          <img className="storm-icon"src={`${forecast?.forecast.forecastday[7].hour[9].condition.icon}`} />
+          <span className="span-tuesday">{dayjs(forecast?.forecast.forecastday[7].hour[9].is_day).format('dddd')}, {dayjs(forecast?.forecast.forecastday[7].date).format("MMM DD")}</span> <br />
+          <span className="span-storm">{forecast?.forecast.forecastday[7].hour[9].condition.text}</span>
+        </div>
+          </div> :<button className="span-days2" onClick={() => setWeather2(true)} >7 days</button>
+        }
+        {
+          weather3?<div className='div10-header'> <div className="div4-header" >
+        <img className="storm-icon"src={`${forecast?.forecast.forecastday[1].hour[9].condition.icon}`} />
+          <span className="span-tuesday">{dayjs(forecast?.forecast.forecastday[1].hour[9].is_day).format('dddd')}, {dayjs(forecast?.forecast.forecastday[1].date).format('MMM DD')}</span> <br />
+          <span className="span-storm">{forecast?.forecast.forecastday[1].hour[9].condition.text}</span>
+        </div>
+        <div className="div4-header">
+          <img className="storm-icon"src={`${forecast?.forecast.forecastday[2].hour[9].condition.icon}`} />
+          <span className="span-tuesday">{dayjs(forecast?.forecast.forecastday[2].hour[9].is_day).format('dddd')}, {dayjs(forecast?.forecast.forecastday[2].date).format("MMM DD")}</span> <br />
+          <span className="span-storm">{forecast?.forecast.forecastday[2].hour[9].condition.text}</span>
+        </div>
+        <div className="div4-header">
+          <img className="storm-icon"src={`${forecast?.forecast.forecastday[3].hour[9].condition.icon}`} />
+          <span className="span-tuesday">{dayjs(forecast?.forecast.forecastday[3].hour[9].is_day).format('dddd')}, {dayjs(forecast?.forecast.forecastday[3].date).format("MMM DD")}</span> <br />
+          <span className="span-storm">{forecast?.forecast.forecastday[3].hour[9].condition.text}</span>
+        </div>
+        <div className="div4-header">
+          <img className="storm-icon"src={`${forecast?.forecast.forecastday[4].hour[9].condition.icon}`} />
+          <span className="span-tuesday">{dayjs(forecast?.forecast.forecastday[4].hour[9].is_day).format('dddd')}, {dayjs(forecast?.forecast.forecastday[4].date).format("MMM DD")}</span> <br />
+          <span className="span-storm">{forecast?.forecast.forecastday[4].hour[9].condition.text}</span>
+        </div>
+        <div className="div4-header">
+          <img className="storm-icon"src={`${forecast?.forecast.forecastday[5].hour[9].condition.icon}`} />
+          <span className="span-tuesday">{dayjs(forecast?.forecast.forecastday[5].hour[9].is_day).format('dddd')}, {dayjs(forecast?.forecast.forecastday[5].date).format("MMM DD")}</span> <br />
+          <span className="span-storm">{forecast?.forecast.forecastday[5].hour[9].condition.text}</span>
+        </div>
+        <div className="div4-header">
+          <img className="storm-icon"src={`${forecast?.forecast.forecastday[6].hour[9].condition.icon}`} />
+          <span className="span-tuesday">{dayjs(forecast?.forecast.forecastday[6].hour[9].is_day).format('dddd')}, {dayjs(forecast?.forecast.forecastday[6].date).format("MMM DD")}</span> <br />
+          <span className="span-storm">{forecast?.forecast.forecastday[6].hour[9].condition.text}</span>
+        </div>
+        <div className="div4-header">
+          <img className="storm-icon"src={`${forecast?.forecast.forecastday[7].hour[9].condition.icon}`} />
+          <span className="span-tuesday">{dayjs(forecast?.forecast.forecastday[7].hour[9].is_day).format('dddd')}, {dayjs(forecast?.forecast.forecastday[7].date).format("MMM DD")}</span> <br />
+          <span className="span-storm">{forecast?.forecast.forecastday[7].hour[9].condition.text}</span>
+        </div>
+        <div className="div4-header">
+          <img className="storm-icon"src={`${forecast?.forecast.forecastday[8].hour[9].condition.icon}`} />
+          <span className="span-tuesday">{dayjs(forecast?.forecast.forecastday[8].hour[9].is_day).format('dddd')}, {dayjs(forecast?.forecast.forecastday[8].date).format("MMM DD")}</span> <br />
+          <span className="span-storm">{forecast?.forecast.forecastday[8].hour[9].condition.text}</span>
+        </div>
+        <div className="div4-header">
+          <img className="storm-icon"src={`${forecast?.forecast.forecastday[9].hour[9].condition.icon}`} />
+          <span className="span-tuesday">{dayjs(forecast?.forecast.forecastday[9].hour[9].is_day).format('dddd')}, {dayjs(forecast?.forecast.forecastday[9].date).format("MMM DD")}</span> <br />
+          <span className="span-storm">{forecast?.forecast.forecastday[9].hour[9].condition.text}</span>
+        </div>
+          </div> :<button className="span-days3" onClick={() => setWeather3(true)} >10 days</button>
+        }
+        
+        </div>
+     
+        
+      
       <div className="header-bash">
         <h1 className='h1-bash' >{forecast?.current.condition.text}</h1>
         <div className="div5-header" >
-          <span className="span-sagat" >{forecast?.forecast.forecastday[0].hour[9].time}</span>
+          <span className="span-sagat" >{dayjs(forecast?.forecast.forecastday[0].hour[9].time).format('hh:mm')}</span>
           <hr />
-          <img className='h-icon'  src={`${forecast?.forecast.forecastday[0].hour[9].condition.icon}`} alt="" />
+          <img className='h-icon'  src={`${forecast?.forecast.forecastday[0].hour[9].condition.icon}`}/>
           <h3 className="h-gradus">{forecast?.forecast.forecastday[0].hour[9].temp_c}℃</h3>
         </div>
         <div className="div5-header" >
-          <span className="span-sagat" >{forecast?.forecast.forecastday[0].hour[10].time}</span>
+          <span className="span-sagat" >{dayjs(forecast?.forecast.forecastday[0].hour[10].time).format('HH:mm')}</span>
           <hr />
           <img className='h-icon' src={`${forecast?.forecast.forecastday[0].hour[10].condition.icon}`} alt="" />
           <h3 className="h-gradus">{forecast?.forecast.forecastday[0].hour[10].temp_c}℃</h3>
         </div>
         <div className="div5-header" >
-          <span className="span-sagat" >{forecast?.forecast.forecastday[0].hour[11].time}</span>
+          <span className="span-sagat" >{dayjs(forecast?.forecast.forecastday[0].hour[11].time).format('HH:mm')}</span>
           <hr />
           <img className='h-icon' src={`${forecast?.forecast.forecastday[0].hour[11].condition.icon}`} alt="" />
           <h3 className="h-gradus">{forecast?.forecast.forecastday[0].hour[11].temp_c}℃</h3>
         </div>
         <div className="div5-header" >
-          <span className="span-sagat" >{forecast?.forecast.forecastday[0].hour[12].time}</span>
+          <span className="span-sagat" >{dayjs(forecast?.forecast.forecastday[0].hour[12].time).format('HH:mm')}</span>
           <hr />
           <img className='h-icon' src={`${forecast?.forecast.forecastday[0].hour[12].condition.icon}`} alt="" />
           <h3 className="h-gradus">{forecast?.forecast.forecastday[0].hour[12].temp_c}℃</h3>
         </div>
         <div className="div5-header" >
-          <span className="span-sagat" >{forecast?.forecast.forecastday[0].hour[13].time}</span>
+          <span className="span-sagat" >{dayjs(forecast?.forecast.forecastday[0].hour[13].time).format('HH:mm')}</span>
           <hr />
           <img className='h-icon' src={`${forecast?.forecast.forecastday[0].hour[13].condition.icon}`} alt="" />
           <h3 className="h-gradus">{forecast?.forecast.forecastday[0].hour[13].temp_c}℃</h3>
         </div>
         <div className="div5-header" >
-          <span className="span-sagat" >{forecast?.forecast.forecastday[0].hour[14].time}</span>
+          <span className="span-sagat" >{dayjs(forecast?.forecast.forecastday[0].hour[14].time).format('HH:mm')}</span>
           <hr />
           <img className='h-icon' src={`${forecast?.forecast.forecastday[0].hour[14].condition.icon}`} alt="" />
           <h3 className="h-gradus"> {forecast?.forecast.forecastday[0].hour[14].temp_c}℃</h3>
